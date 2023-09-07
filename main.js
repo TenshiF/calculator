88cf173d4851d7c98687e86622d0715721051086
TenshiF <122945396+TenshiF@users.noreply.github.com.>
2023-09-07 14:33:47 -0400

    it might not look good, but we done boys

--- main.js
+++ main.js
@@ -11,12 +11,8 @@ function multiply (a,b) {
 }
 
 function divide (a,b) {
-    return parseFloat(a/b);
+    return parseFloat((a/b).toFixed(3));
 }
-
-let firstNum;
-let operator;
-let secondNum;
 
 function operate (first, op, second) {
     let result;
@@ -27,7 +23,7 @@ function operate (first, op, second) {
         case '-':
             result = subtract(first,second);
             break;
-        case '*':
+        case 'x':
             result = multiply(first,second);
             break;
         case '/':
@@ -36,8 +32,69 @@ function operate (first, op, second) {
     }
     return result;
 }
+let firstNum;
+let operator;
+let secondNum;
 
-console.log(operate(2,'+',3));
-console.log(operate(2,'-',3));
-console.log(operate(2,'*',3));
-console.log(operate(2,'/',3));
+let haveFirstNumber = false;
+let haveOperator = false;
+let haveSecondNumber = false;
+
+display = document.querySelector('.display');
+
+nButtons = document.querySelectorAll('.number-button');
+
+nButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!haveFirstNumber) {
+            display.textContent += button.textContent;
+        }
+        else if (haveOperator) {
+            if (parseInt(display.textContent) == firstNum) {
+                display.textContent = button.textContent;
+            }
+            else {
+                display.textContent += button.textContent;
+            }
+        }
+        
+    })
+})
+
+oButtons = document.querySelectorAll('.operator-button');
+
+oButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!haveFirstNumber) {
+            firstNum = parseInt(display.textContent);
+            haveFirstNumber = true;
+            operator = button.textContent;
+            haveOperator = true;
+        }
+        else {
+            secondNum = parseInt(display.textContent);
+            firstNum = operate(firstNum,operator,secondNum);
+            operator = button.textContent;
+            console.log(firstNum);
+            display.textContent = firstNum;
+        }
+        
+    })
+})
+
+eButton = document.querySelector('.equal-button');
+eButton.addEventListener('click', function() {
+    if (haveFirstNumber && haveOperator) {
+        secondNum = parseInt(display.textContent);
+        firstNum = operate(firstNum,operator,secondNum);
+        console.log(firstNum);
+        display.textContent = firstNum;
+    }
+})
+
+cButton = document.querySelector('.clear-button');
+cButton.addEventListener('click', function() {
+    display.textContent = '';
+    haveFirstNumber = false;
+    haveOperator = false;
+})
